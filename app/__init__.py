c19dc919c0f2dd271b99615607de33bd5b8cb907
seuen <sz1795@nyu.edu>
2016-06-16 21:08:39 +0000

    link database

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,5 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Event
+# 
+from models import Test
--- app/models.py
+++ app/models.py
@@ -1,17 +1,28 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
+import sqlalchemy
 
-
-class Event(db.Model):
+# this class used to initialize table datatypes,
+# which has been done in folder raw_data_processing/to_database.py
+class Test(db.Model):
     __tablename__ = 'test'
 
-    id = db.Column(db.Integer, primary_key=True)
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
+    # id = db.Column(db.Integer, primary_key=True)
+    # result_all = db.Column(JSON)
+    # result_no_stop_words = db.Column(JSON)
+              
+    # dtDict = {'EVENT_ID': sqlalchemy.types.NUMERIC,
+    #       'DATETIME_FROM':sqlalchemy.types.DateTime,
+    #       'DATETIME_TO':sqlalchemy.types.DateTime,
+    #       'LOC_X': sqlalchemy.types.NUMERIC,
+    #       'LOC_Y': sqlalchemy.types.NUMERIC,
+    #       'PR_CODE':sqlalchemy.types.NUMERIC,
+    #       'BLD_TYPE':sqlalchemy.types.String
+    #       }          
 
-    def __init__(self, result_all, result_no_stop_words):
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
+    # def __init__(self, result_all, result_no_stop_words):
+    #     self.result_all = result_all
+    #     self.result_no_stop_words = result_no_stop_words
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<EVENT_ID {}>'.format(self.EVENT_ID)
