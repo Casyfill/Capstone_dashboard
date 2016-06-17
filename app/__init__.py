cdf975227b3bbeb6f27ba10f0a4d485e804546f1
seuen <sz1795@nyu.edu>
2016-06-17 12:31:43 +0000

    database

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-from models import Test
+# from models import Test
--- app/models.py
+++ app/models.py
@@ -4,33 +4,33 @@ import sqlalchemy
 
 # this class used to initialize table datatypes,
 # which has been done in folder raw_data_processing/to_database.py
-class Test(db.Model):
-    __tablename__ = 'test'
+# class Test(db.Model):
+#     __tablename__ = 'test'
 
-    eventid = db.Column(db.NUMERIC, primary_key=True)
-    datetimefrom = db.Column(db.DateTime)
-    datetimeto = db.Column(db.DateTime)
-    locx = db.Column(db.NUMERIC)
-    locy = db.Column(db.NUMERIC)
-    prcode = db.Column(db.NUMERIC)
-    bldtype = db.Column(db.String)
+#     eventid = db.Column(db.NUMERIC, primary_key=True)
+#     datetimefrom = db.Column(db.DateTime)
+#     datetimeto = db.Column(db.DateTime)
+#     locx = db.Column(db.NUMERIC)
+#     locy = db.Column(db.NUMERIC)
+#     prcode = db.Column(db.NUMERIC)
+#     bldtype = db.Column(db.String)
     
-    # dtDict = {'eventid': sqlalchemy.types.NUMERIC,
-    #       'datetime from':sqlalchemy.types.DateTime,
-    #       'DATETIME_TO':sqlalchemy.types.DateTime,
-    #       'LOC_X': sqlalchemy.types.NUMERIC,
-    #       'LOC_Y': sqlalchemy.types.NUMERIC,
-    #       'PR_CODE':sqlalchemy.types.NUMERIC,
-    #       'BLD_TYPE':sqlalchemy.types.String
-    #       }          
+#     # dtDict = {'eventid': sqlalchemy.types.NUMERIC,
+#     #       'datetime from':sqlalchemy.types.DateTime,
+#     #       'DATETIME_TO':sqlalchemy.types.DateTime,
+#     #       'LOC_X': sqlalchemy.types.NUMERIC,
+#     #       'LOC_Y': sqlalchemy.types.NUMERIC,
+#     #       'PR_CODE':sqlalchemy.types.NUMERIC,
+#     #       'BLD_TYPE':sqlalchemy.types.String
+#     #       }          
 
-    def __init__(self, DATETIME_FROM, DATETIME_TO, LOC_X, LOC_Y, PR_CODE, BLD_TYPE):
-        self.datetimefrom = DATETIME_FROM
-        self.datetimefrom = DATETIME_TO
-        self.locx = LOC_X
-        self.locx = LOC_Y
-        self.prcode = PR_CODE
-        self.bldtype = BLD_TYPE
+#     def __init__(self, DATETIME_FROM, DATETIME_TO, LOC_X, LOC_Y, PR_CODE, BLD_TYPE):
+#         self.datetimefrom = DATETIME_FROM
+#         self.datetimefrom = DATETIME_TO
+#         self.locx = LOC_X
+#         self.locx = LOC_Y
+#         self.prcode = PR_CODE
+#         self.bldtype = BLD_TYPE
         
-    def __repr__(self):
-        return '<EVENT_ID {}>'.format(self.EVENT_ID)
+#     def __repr__(self):
+#         return '<EVENT_ID {}>'.format(self.EVENT_ID)
